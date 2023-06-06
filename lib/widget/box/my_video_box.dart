6991ac1324fbdb473dbfc0fadbaef7408e5632a4
金眉卲 <120027289+Jinmeishao@users.noreply.github.com>
2023-06-07 01:22:09 +0900

    20230607 동영상 업로드 수정

--- lib/widget/box/my_video_box.dart
+++ lib/widget/box/my_video_box.dart
@@ -16,8 +16,16 @@ class MyVideoBox extends StatelessWidget{
   final String type; // 다이내믹, 밸런스, 파워, 지구력 등 유형
   final double replyCnt; // 댓글 개수
   final double likeCnt; // 좋아요 개수
-  final double width; // 가로 사이즈 기본값
-  final double height; // 세로 사이즈 기본값
+  final double x; //기기화면 전체 가로 길이 대비 만들고 싶은 가로 길이 비율 ex) 1 → 가로 꽉 채우는 경우
+  final double y; //기화면 전체 높이 길이 대비 만들고 싶은 높이 길이 비율 ex) 0.8
+  final double pl; // 기기화면 전체 가로길이 대비 만들고 싶은 내부 패딩 비율 ex) 값 0.05 → 전체 길이 * 0.05 사용해서 실제 왼쪽 패딩 추출
+  final double pr; // 기기화면 전체 가로길이 대비 만들고 싶은 내부 패딩 비율 ex) 값 0 → 전체 길이 * 0 사용해서 실제 오른쪽 패딩 0으로 적용
+  final double pt; // 기기화면 전체 세로길이 대비 만들고 싶은 내부 패딩 비율
+  final double pb; // 기기화면 전체 세로길이 대비 만들고 싶은 내부 패딩 비율
+  final double ml; // 기기화면 전체 가로길이 대비 만들고 싶은 왼쪽 마진 패딩 비율
+  final double mr; // 기기화면 전체 가로길이 대비 만들고 싶은 오른쪽 마진 비율
+  final double mt; // 기기화면 전체 세로길이 대비 만들고 싶은 위쪽 마진 비율
+  final double mb; // 기기화면 전체 세로길이 대비 만들고 싶은 아래쪽 마진 비율
 
   MyVideoBox({
     Key? key,
@@ -31,36 +39,47 @@ class MyVideoBox extends StatelessWidget{
     this.type = '피카츄',
     this.replyCnt = 0,
     this.likeCnt = 0,
-    this.width = 280,
-    this.height = 80,
+    this.x = 1, // 가로 요청 비율 안넘어오는 경우 기본값
+    this.y = 0.15,  // 세로 요청 비율 안넘어오는 경우 기본값
+    this.pl = 0,
+    this.pr = 0,
+    this.pt = 0,
+    this.pb = 0,
+    this.ml = 0,
+    this.mr = 0,
+    this.mt = 0.012,
+    this.mb = 0.012,
   }):super(key:key);
 
   @override
   Widget build(BuildContext context){
+
+    final screenWidth = MediaQuery.of(context).size.width; // 기기 전체 가로 길이 → 매번 호출하지 않고, 한 파일에서 관리해도 될듯
+    final screenHeight = MediaQuery.of(context).size.height; // 기기 전체 세로 길이  → 매번 호출하지 않고, 한 파일에서 관리해도 될듯
+
     return Container(
+      height: screenHeight * y,
+      width: double.infinity, // Occupy full width 버튼 가로길이 현재 가능 영역에서 꽉차게 그리기
+      color: Colors.red,
       margin: EdgeInsets.only(
-        top: 0,
-        bottom: 10.0,
-        left: 10.0,
-        right: 10.0,
+        top: screenHeight * this.mt,
+        bottom: screenHeight * this.mb,
+        left: 0,
+        right: 0,
       ),
-      width: this.width,
-      height: this.height,
       //color: Color.fromARGB(255, 252, 249, 81), // TODO 제거 예정. 내부 요소 레이아웃 확인하기 위해 임시로 색상 적용함.
       child: Row(
           children: [
-            Container( // 동영상 섬네일.
-                height: 70, width: 70,
+            AspectRatio( // 동영상 섬네일  TODO 이미지 추후에 붙이기
+              aspectRatio: 1, // Set the aspect ratio to 1:1 for a square shape
+              child: Container(
                 color: Colors.purple,
-                margin: EdgeInsets.only(
-                  top: 5.0,
-                  bottom: 5.0,
-                  left: 5.0,
-                  right: 5.0,
-                ),
+              )
             ),
-            Container( // 동영상 관련 상세 정보 상자 → 날짜 제목 등
-              height: 70, width: 195,
+            Container( // 동영상 섬네일 우측
+              //width: double.infinity, // Occupy full width 현재 가능 영역에서 꽉차게 그리기
+              height: 70,
+              color: Colors.brown,
               margin: EdgeInsets.only(
                 top: 5.0,
                 bottom: 5.0,
